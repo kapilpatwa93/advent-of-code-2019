1f21234808c3e3f1d9f43dac8df431af21300d5f
Kapil Patwa <kapil.p@peopleinteractive.in>
2019-12-11 15:26:05 +0530

    Added solution for day4 part1 and part2

--- Day4/part2.js
+++ Day4/part2.js
@@ -1,12 +1,25 @@
 function isValidPassword(password) {
-    password = password.toString()
+    password = password.toString();
     let sameAdjacentPresent = false;
+    let prevSame = false;
+    let moreThan2 = false;
     let isValid = true;
     for (let i = 1; i < password.length; i++) {
         const prevChar = password.substr(i - 1, 1);
         const currChar = password.substr(i, 1);
         if (prevChar === currChar) {
-            sameAdjacentPresent = true
+            if (prevSame) {
+                moreThan2 = true;
+            }
+            prevSame = true;
+
+        } else {
+            if (prevSame && !moreThan2) {
+                sameAdjacentPresent = true;
+            }
+            prevSame = false;
+            moreThan2 = false;
+
         }
         if (prevChar > currChar) {
             isValid = false;
@@ -14,21 +27,26 @@ function isValidPassword(password) {
         }
 
     }
+    if (prevSame && !moreThan2) {
+        sameAdjacentPresent = true;
+    }
     if (!sameAdjacentPresent) {
         isValid = false;
     }
-    return isValid
+    return isValid;
 }
 
 function main() {
     const start = 271973;
     const end = 785961;
     let totalIsValid = 0;
-    for (let i = start; i<= end; i++) {
-        if(isValidPassword(i)) {
+    for (let i = start; i <= end; i++) {
+
+        if (isValidPassword(i)) {
             totalIsValid++;
         }
     }
     console.log(totalIsValid)
 }
+
 main();
